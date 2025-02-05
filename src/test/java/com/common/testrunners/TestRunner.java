e8e0b50f177fe2c17d3acdc02dfc41ab7bd16df7
Renuka Sharma <renukasharma25sept@gmail.com>
2025-02-06 03:04:05 +0530

    Renuka | updated the css html

---
+++ src/test/java/com/common/testrunners/TestRunner.java
@@ -0,0 +1,20 @@
+package com.common.testrunners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features", // Path to the feature files
+        glue = {"com.myntra.stepdefs", "com.common"},   // Package containing step definition classes
+        plugin = {
+                "pretty",                            // Prints Gherkin steps in the console
+                "html:target/cucumber-reports.html", // Generates an HTML report
+                "json:target/cucumber-reports.json"  // Generates a JSON report
+        },
+        monochrome = true,                       // Makes console output more readable
+        tags = "@SmokeTest"                      // Executes scenarios tagged with @SmokeTest
+)
+public class TestRunner {
+}
